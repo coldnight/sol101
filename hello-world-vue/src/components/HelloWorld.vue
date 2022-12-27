6f626533075702cfa8ca1887c7684d052f00b4c9
wanghui <grayking.w@gmail.com>
2022-12-27 11:59:57 +0800

    feat: counter

--- hello-world-vue/src/components/HelloWorld.vue
+++ hello-world-vue/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script>
 import * as web3 from "@solana/web3.js";
 import { Buffer } from "buffer";
-import { u8, seq } from "@solana/buffer-layout";
+import { u32 } from "@solana/buffer-layout";
 
 export default {
   name: 'HelloWorld',
@@ -15,19 +15,34 @@ export default {
       const secretKey = Uint8Array.from([39,200,223,129,7,187,231,0,60,139,213,134,61,123,105,106,105,183,119,59,4,187,86,82,218,182,26,220,232,117,148,39,127,193,26,147,18,125,138,125,102,27,211,194,53,168,55,93,110,138,37,182,78,146,199,46,134,250,130,223,48,218,61,202]);
       const keypair = web3.Keypair.fromSecretKey(secretKey);
       console.log(keypair.publicKey.toBase58());
+      const programId = new web3.PublicKey("Cj9VjDrohXuz914FNbaTMmEqYXkLkJKZn9FCq4UGGGwr");
+
+      const [pda, bump] = web3.PublicKey.findProgramAddressSync(
+        [
+          Buffer.from("counter-seeds"),
+        ],
+        programId,
+      );
+      console.log("pda", pda.toBase58(), ", bump", bump);
 
       const connection = new web3.Connection(web3.clusterApiUrl("testnet"));
 
-      const instruction_t = seq(u8(), 4);
-      const data = Buffer.alloc(instruction_t.span);
+      const instruction_t = u32();
+      const insData = Buffer.alloc(instruction_t.span);
 
-      instruction_t.encode([1, 2, 3, 4], data);
-
+      instruction_t.encode(1, insData);
+      const bumpData = Buffer.from(Uint8Array.of(bump));
+      const data = Buffer.concat([insData, bumpData], insData.length + bumpData.length);
+      console.log("data", data);
       const tx = new web3.Transaction();
       tx.add(
         new web3.TransactionInstruction({
-          keys: [{pubkey: keypair.publicKey, isSigner: true, isWritable: true}],
-          programId: "2V7fLp61TPBb1Xi728H5xUNpPkAVoEMZv58wHFtRwNf2",
+          keys: [
+            {pubkey: keypair.publicKey, isSigner: true, isWritable: true},
+            {pubkey: pda, isSigner: false, isWritable: true},
+            {pubkey: web3.SystemProgram.programId, isSigner: false, isWritable: false},
+          ],
+          programId: programId,
           data,
         }),
       );
--- hello-world/src/lib.rs
+++ hello-world/src/lib.rs
@@ -1,9 +1,16 @@
+use std::borrow::Borrow;
+
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint,
-    entrypoint::{self, ProgramResult},
+    entrypoint::ProgramResult,
     msg,
+    program::invoke_signed,
+    program_error::ProgramError,
     pubkey::Pubkey,
+    rent::Rent,
+    system_instruction,
+    sysvar::Sysvar,
 };
 
 entrypoint!(process_instruction);
@@ -14,5 +21,55 @@ pub fn process_instruction(
     instruction_data: &[u8],
 ) -> ProgramResult {
     msg!("Hello, world!");
+
+    let delta = u32::from_le_bytes(
+        instruction_data[0..4]
+            .try_into()
+            .or(Err(ProgramError::InvalidInstructionData))?,
+    );
+    let (bump, _) = instruction_data[4..]
+        .split_first()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+    let accounts_iter = &mut accounts.iter();
+    let funding_account = next_account_info(accounts_iter)?;
+    let pda_account = next_account_info(accounts_iter)?;
+    let system_program = next_account_info(accounts_iter)?;
+
+    if pda_account.try_borrow_data()?.len() < 4 {
+        let new_size: usize = 4;
+        // Initialize pda.
+        let lamports_required = Rent::get()?.minimum_balance(new_size);
+        let create_pda_account_ix = system_instruction::create_account(
+            funding_account.key,
+            pda_account.key,
+            lamports_required,
+            new_size as u64,
+            program_id,
+        );
+        let signer_seeds: &[&[u8]; 2] = &[b"counter-seeds", &[*bump]];
+
+        invoke_signed(
+            &create_pda_account_ix,
+            &[
+                funding_account.clone(),
+                pda_account.clone(),
+                system_program.clone(),
+            ],
+            &[signer_seeds],
+        )?;
+    }
+    let mut counter = u32::from_le_bytes(
+        pda_account.try_borrow_data()?[0..4]
+            .try_into()
+            .or(Err(ProgramError::InvalidAccountData))?,
+    );
+    msg!("old counter value is {}", counter);
+    counter += delta;
+    msg!("now counter value is {}", counter);
+    let byts = counter.to_le_bytes();
+    let mut data = pda_account.try_borrow_mut_data()?;
+    for i in 0..4 {
+        data[i] = byts[i];
+    }
     Ok(())
 }
